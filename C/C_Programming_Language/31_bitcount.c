189a3e798ac4e71962d32c920702dc681f3c1754
Quan Do <minhquand3@gmail.com>
2023-06-11 21:11:24 +0700

    C Programming Language - 2.10 Fix

--- C/C_Programming_Language/31_bitcount.c
+++ C/C_Programming_Language/31_bitcount.c
@@ -13,7 +13,6 @@ int bitcount(unsigned x) {
   // x &= (x - 1) deletes the rightmost 1-bit in x
   // This is because x - 1 flips the rightmost 1-bit and all bits to the right
   for (b = 0; x != 0; x &= (x - 1)) {
-    printf("x: %x, b: %d\n", x, b);
     b++;
   }
   return b;
