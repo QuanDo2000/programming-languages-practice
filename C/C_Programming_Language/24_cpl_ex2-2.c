e762ef4b7f9b624cdef058d6de52787c1ecbc117
Quan Do <minhquand3@gmail.com>
2023-06-11 12:12:41 +0700

    C Programming Language - 2.6

---
+++ C/C_Programming_Language/24_cpl_ex2-2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int main() {
+  int c, i, lim;
+  char s[MAXLINE];
+
+  lim = MAXLINE;
+  for (i = 0; i < lim - 1; ++i) {
+    if ((c = getchar()) == '\n') {
+      break;
+    } else if (c == EOF) {
+      break;
+    } else {
+      s[i] = c;
+    }
+  }
+  s[i] = '\0';
+  printf("%s\n", s);
+}
