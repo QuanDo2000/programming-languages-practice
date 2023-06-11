3b72928dadb1af83686940a7d9abe62a3e6d9e11
Quan Do <minhquand3@gmail.com>
2023-06-11 18:35:32 +0700

    C Programming Language - 2.7

---
+++ C/C_Programming_Language/25_htoi.c
@@ -0,0 +1,46 @@
+#include <ctype.h>
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+int getline(char s[], int lim);
+int htoi(char s[]);
+
+int main() {
+  char s[MAXLINE];
+  getline(s, MAXLINE);
+  printf("%d\n", htoi(s));
+  return 0;
+}
+
+int getline(char s[], int lim) {
+  int c, i;
+  for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+    s[i] = c;
+  s[i] = '\0';
+  return i;
+}
+
+int htoi(char s[]) {
+  int i, n, c;
+  i = 0;
+  n = 0;
+  if (s[i] == '0') {
+    ++i;
+    if (s[i] == 'x' || s[i] == 'X')
+      ++i;
+  }
+  while (s[i] != '\0') {
+    if (isdigit(s[i]))
+      c = s[i] - '0';
+    else if (s[i] >= 'a' && s[i] <= 'f')
+      c = s[i] - 'a' + 10;
+    else if (s[i] >= 'A' && s[i] <= 'F')
+      c = s[i] - 'A' + 10;
+    else
+      return -1;
+    n = 16 * n + c;
+    ++i;
+  }
+  return n;
+}
