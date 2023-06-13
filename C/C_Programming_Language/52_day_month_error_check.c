332aae9ec51dba691aacbba4a07bc6bc9782b90e
Quan Do <minhquand3@gmail.com>
2023-06-13 17:17:56 +0700

    C Programming Language - 5.9

--- C/C_Programming_Language/52_day_month_error_check.c
+++ C/C_Programming_Language/52_day_month_error_check.c
@@ -1,3 +1,20 @@
+#include <stdio.h>
+
+int day_of_year(int year, int month, int day);
+void month_day(int year, int yearday, int *pmonth, int *pday);
+
+int main() {
+  int year, month, day, yearday;
+  year = 2020;
+  month = 2;
+  day = 29;
+  yearday = day_of_year(year, month, day);
+  printf("%d-%d-%d is the %dth day of %d\n", year, month, day, yearday, year);
+  month_day(year, yearday, &month, &day);
+  printf("the %dth day of %d is %d-%d-%d\n", yearday, year, year, month, day);
+  return 0;
+}
+
 static char daytab[2][13] = {
     {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31},
     {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}};
@@ -9,12 +26,12 @@ int day_of_year(int year, int month, int day) {
     printf("error: month must be between 1 and 12\n");
     return -1;
   }
-  if (day < 1 || day > daytab[leap][month]) {
-    printf("error: day must be between 1 and %d\n", daytab[leap][month]);
+  if (day < 1 || day > *(*(daytab + leap) + month)) {
+    printf("error: day must be between 1 and %d\n", *(*(daytab + leap) + month));
     return -1;
   }
   for (i = 1; i < month; i++) {
-    day += daytab[leap][i];
+    day += *(*(daytab + leap) + i);
   }
   return day;
 }
@@ -26,8 +43,8 @@ void month_day(int year, int yearday, int *pmonth, int *pday) {
     printf("error: yearday must be between 1 and %d\n", leap ? 366 : 365);
     return;
   }
-  for (i = 1; yearday > daytab[leap][i]; i++) {
-    yearday -= daytab[leap][i];
+  for (i = 1; yearday > *(*(daytab + leap) + i); i++) {
+    yearday -= *(*(daytab + leap) + i);
   }
   *pmonth = i;
   *pday = yearday;
